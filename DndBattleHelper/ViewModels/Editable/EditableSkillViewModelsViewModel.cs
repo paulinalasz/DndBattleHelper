6bde8ae8d7e123ad06c8d990467ddf7af9a73454
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2024-09-30 15:39:18 +0800

    Add Remove EditableSkillViewModel Captabilities

--- DndBattleHelper/ViewModels/Editable/EditableSkillViewModelsViewModel.cs
+++ DndBattleHelper/ViewModels/Editable/EditableSkillViewModelsViewModel.cs
@@ -1,14 +1,53 @@
-﻿using System.Collections.ObjectModel;
+﻿using DndBattleHelper.Helpers;
+using System.Collections.ObjectModel;
 
 namespace DndBattleHelper.ViewModels.Editable
 {
-    public class EditableSkillViewModelsViewModel
+    public class EditableSkillViewModelsViewModel : NotifyPropertyChanged
     {
-        public ObservableCollection<EditableSkillViewModel> EditableSkillViewModels { get; set; }
+        public ObservableCollection<EditableSkillViewModel> EditableSkillViewModels { get; }
 
         public EditableSkillViewModelsViewModel(ObservableCollection<EditableSkillViewModel> editableSkillViewModels)
         {
             EditableSkillViewModels = editableSkillViewModels;
+            foreach (var viewModel in EditableSkillViewModels)
+            {
+                viewModel.Removed += () =>
+                {
+                    Remove(viewModel);
+                };
+            }
+
+            EditableSkillViewModels.CollectionChanged += EditableSkillViewModels_CollectionChanged;
+        }
+
+        private void EditableSkillViewModels_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
+                    foreach (var viewModel in EditableSkillViewModels)
+                    {
+                        viewModel.Removed += () =>
+                        {
+                            Remove(viewModel);
+                        };
+                    }
+                    break;
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
+                    break;
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Replace:
+                    break;
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Move:
+                    break;
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Reset:
+                    break;
+            }
+        }
+
+        public void Remove(EditableSkillViewModel viewModel) 
+        {
+            EditableSkillViewModels.Remove(viewModel);
         }
 
     }
