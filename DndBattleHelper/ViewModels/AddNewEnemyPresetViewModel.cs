682a705d01112315f6bc54ce2369734393270261
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2024-10-08 18:13:40 +0800

    Clean up

---
+++ DndBattleHelper/ViewModels/AddNewEnemyPresetViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DndBattleHelper.Models;
+using DndBattleHelper.ViewModels.Editable;
+using DndBattleHelper.ViewModels.Providers;
+
+namespace DndBattleHelper.ViewModels
+{
+    public class AddNewEnemyPresetViewModel : NewEnemyViewModel
+    {
+        public Enemy AddedEnemyPreset { get; set; }
+
+        public AddNewEnemyPresetViewModel(TargetArmourClassProvider targetArmourClassProvider, AdvantageDisadvantageProvider advantageDisadvantageProvider) : base(targetArmourClassProvider, advantageDisadvantageProvider)
+        {
+        }
+
+        public override bool IsRollHealthVisible => false;
+
+        public override void CreateNewEnemy()
+        {
+            AddedEnemyPreset = new EnemyFactory().Create(
+                            Name,
+                            ArmourClass,
+                            Health,
+                            Speed,
+                            Strength,
+                            Dexterity,
+                            Constitution,
+                            Intelligence,
+                            Wisdom,
+                            Charisma,
+                            EditSkillsViewModel.CopyNewModels(),
+                            EditSensesViewModel.CopyNewModels(),
+                            PassivePerception.CopyModel(),
+                            EditLanguagesViewModel.CopyNewModels(),
+                            ChallengeRatingViewModel.CopyModel(),
+                            EditAbilitiesViewModel.CopyNewModels(),
+                            EditActionsViewModel.CopyNewModels());
+        }
+    }
+}
