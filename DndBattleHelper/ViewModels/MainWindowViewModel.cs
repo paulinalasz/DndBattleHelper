1897ea9517e36c1040df7b11647f02cbfc2dff7e
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2024-10-09 10:29:29 +0800

    Remove Entities in initiative set up from Main WIndow ViewMOdel

--- DndBattleHelper/ViewModels/MainWindowViewModel.cs
+++ DndBattleHelper/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
 using DndBattleHelper.Models;
-using DndBattleHelper.Models.Enums;
-using DndBattleHelper.Models.ActionTypes;
 using DndBattleHelper.Helpers;
-using DndBattleHelper.ViewModels.Editable.Actions;
 using DndBattleHelper.Helpers.DialogService;
 using DndBattleHelper.ViewModels.Providers;
-using System;
 
 namespace DndBattleHelper.ViewModels
 {
@@ -30,56 +26,7 @@ namespace DndBattleHelper.ViewModels
 
             _dialogService = dialogService;
 
-            var skills = new List<Skill>();
-            var senses = new List<SenseType>();
-            var languages = new List<LanguageType>();
-            var abilities = new List<Ability>();
-            var actions = new List<EntityAction>();
-
-            skills.Add(new Skill(SkillType.Perception, new Modifier(ModifierType.Plus, 7)));
-            skills.Add(new Skill(SkillType.Insight, new Modifier(ModifierType.Plus, 9)));
-
-            senses.Add(SenseType.Darkvision60ft);
-
-            var passivePerception = new PassivePerception(17);
-
-            languages.Add(LanguageType.Common);
-            languages.Add(LanguageType.Abyssal);
-
-            abilities.Add(new Ability("Charge", "If the minotaur moves at least 10 ft. straight toward a target and then hits it with a gore attack on the same turn, the target takes an extra 9 (2d8) piercing damage. If the target is a creature, it must succeed on a DC 14 Strength saving throw or be pushed up to 10 ft. away and knocked prone."));
-            abilities.Add(new Ability("name", "desc"));
-
-            var damageRolls = new List<DamageRoll>
-            {
-                new DamageRoll
-                (2,
-                12,
-                new Modifier(ModifierType.Plus, 4),
-                DamageType.Slashing),
-                new DamageRoll
-                (1, 6, new Modifier(ModifierType.Plus, 1), DamageType.Cold)
-            };
-
-            actions.Add(new AttackAction("Greataxe", "Melee Weapon Attack: +6 to hit, reach 5 ft., one target. Hit: (2d12 + 4) slashing damage.",
-                ActionCost.MainAction, damageRolls, new Modifier(ModifierType.Plus, 6)));
-            actions.Add(new AttackAction("name", "test", ActionCost.MainAction, damageRolls, new Modifier(ModifierType.Plus, 6)));
-
-            var actionViewModels = new ObservableCollection<EntityActionViewModel>();
-            foreach(var action in actions)
-            {
-                actionViewModels.Add(new AttackActionViewModel((AttackAction)action, _targetArmourClassProvider, _advantageDisadvantageProvider));
-            }
-
-            var challengeRating = new ChallengeRating(ChallengeRatingLevel.Three);
-
-            var enemy1 = new Enemy("Minotaur 1", 10, 70, 30, 10, 10, 10, 10, 10, 10, skills, senses, passivePerception, languages, challengeRating, abilities, actions);
-            var enemy2 = new Enemy("Minotaur 2", 10, 70, 30, 10, 10, 10, 10, 10, 10, skills, senses, passivePerception, languages, challengeRating, abilities, actions);
-            var player1 = new Player("Bar", 132);
-
-            EntitiesInInitiative = [new EnemyViewModel(enemy1, actionViewModels, _targetArmourClassProvider, _advantageDisadvantageProvider), 
-                new EnemyViewModel(enemy2, actionViewModels, _targetArmourClassProvider, _advantageDisadvantageProvider),
-                new PlayerViewModel(player1),
-            ];
+            EntitiesInInitiative = new ObservableCollection<EntityViewModel>();
 
             TurnNumber = 0;
             SelectedTab = TurnNumber;
@@ -142,7 +89,6 @@ namespace DndBattleHelper.ViewModels
             SelectedTab = TurnNumber;
         }
 
-        private int _displayTurnNumber;
         public int DisplayTurnNumber => TurnNumber + 1;
 
         public int EntitiesInInitiativeCount => EntitiesInInitiative?.Count ?? 0;
