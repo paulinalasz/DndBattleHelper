2011721f2a0f136bb108a446f6c7d2927133b222
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2024-10-16 11:04:47 +0800

    Fix issue with damage rolls not clearing after adding

--- DndBattleHelper/ViewModels/Editable/EditDamageRollsViewModel.cs
+++ DndBattleHelper/ViewModels/Editable/EditDamageRollsViewModel.cs
@@ -64,6 +64,7 @@ namespace DndBattleHelper.ViewModels.Editable
             DamageModifierViewModel.ModifierType = 0;
             DamageModifierViewModel.ModifierValue = 0;
             SelectedDamageType = 0;
+            EditableTraitViewModelsViewModel.EditableTraitViewModels.Clear();
         }
 
         public List<DamageRoll> CopyNewModels()
