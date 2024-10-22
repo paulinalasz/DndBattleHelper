fb5632fb3f25aee6b209935bf2ed4fa479f06b80
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2024-10-22 19:14:56 +0800

    Failed screen propoirtioanl messing aboutness

--- DndBattleHelper/Views/EnemyView.xaml.cs
+++ DndBattleHelper/Views/EnemyView.xaml.cs
@@ -1,18 +1,6 @@
-﻿using DndBattleHelper.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DndBattleHelper.Helpers;
 
 namespace DndBattleHelper.Views
 {
@@ -21,9 +9,71 @@ namespace DndBattleHelper.Views
     /// </summary>
     public partial class EnemyView : UserControl
     {
+        private bool _isResizing;
+        public double previousMaxWidth1;
+        public double previousMaxWidth2;
+
         public EnemyView()
         {
             InitializeComponent();
+            //previousMaxWidth1 = 0;
+            //previousMaxWidth2 = 0; 
+            //ParentGrid.SizeChanged += MainGrid_SizeChanged; // Subscribe to SizeChanged event
+        }
+
+        //MAYBE TODO
+        //private void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    // Prevent recursive calls to the event handler
+        //    if (_isResizing)
+        //        return;
+
+        //    try
+        //    {
+        //        _isResizing = true; // Set the flag to true to prevent further execution
+
+        //        // Set columns to Auto
+        //        FirstColumn.Width = new GridLength(1, GridUnitType.Auto);
+        //        ActionColumn.Width = new GridLength(1, GridUnitType.Auto);
+
+        //        // Force layout update to calculate the Auto width
+        //        ParentGrid.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+        //        ParentGrid.Arrange(new Rect(0, 0, ParentGrid.DesiredSize.Width, ParentGrid.DesiredSize.Height));
+
+        //        // Capture the calculated Auto widths
+
+        //        if(Maths.AreAlmostEqual(previousMaxWidth1, First )
+
+        //        previousMaxWidth1 = FirstColumn.ActualWidth;
+        //        previousMaxWidth2 = ActionColumn.ActualWidth;
+
+        //        // Set the MaxWidth based on the calculated Auto width
+        //        FirstColumn.MaxWidth = previousMaxWidth1;
+        //        ActionColumn.MaxWidth = previousMaxWidth2;
+
+        //        // Restore columns to Star sizing
+        //        FirstColumn.Width = new GridLength(1, GridUnitType.Star);
+        //        ActionColumn.Width = new GridLength(1, GridUnitType.Star);
+
+        //        // Force layout update again to reapply the sizing
+        //        ParentGrid.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+        //        ParentGrid.Arrange(new Rect(0, 0, ParentGrid.DesiredSize.Width, ParentGrid.DesiredSize.Height));
+        //    }
+        //    finally
+        //    {
+        //        _isResizing = false; // Reset the flag after layout processing is done
+        //    }
         }
     }
-}
+
+        //private void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    // Get the actual width of the second column (ActionColumn)
+        //    var availableWidth = ParentGrid.ActualWidth - ActionColumn.ActualWidth;
+
+        //    // Set the MaxWidth of the first column (AutoColumn) to match the available width
+        //    FirstColumn.MaxWidth = availableWidth;
+        //    //FirstColumn.Width = new GridLength(availableWidth);
+        //}
+
+
