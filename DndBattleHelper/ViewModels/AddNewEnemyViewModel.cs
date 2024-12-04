d533d1d0f6a8906be460984a9542736471d92972
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2024-12-04 21:31:45 +0800

    Move edit preset section to NewEnemyViewModel. Now can edit presets

--- DndBattleHelper/ViewModels/AddNewEnemyViewModel.cs
+++ DndBattleHelper/ViewModels/AddNewEnemyViewModel.cs
@@ -1,11 +1,6 @@
-﻿using DndBattleHelper.Helpers;
-using System.Windows.Input;
-using DndBattleHelper.Models;
+﻿using DndBattleHelper.Models;
 using DndBattleHelper.ViewModels.Editable;
 using DndBattleHelper.ViewModels.Providers;
-using DndBattleHelper.ViewModels.Editable.Traits;
-using DndBattleHelper.Helpers.DialogService;
-using System.Windows.Navigation;
 
 namespace DndBattleHelper.ViewModels
 {
@@ -28,68 +23,6 @@ namespace DndBattleHelper.ViewModels
             SameInitiative = true;
             SameHealth = false;
         }
-
-        //#region Apply Presets
-        //public List<EnemyPreset> EnemyPresets => _presets.EnemyPresets;
-
-        //private EnemyPreset _selectedEnemyPreset;
-        //public EnemyPreset SelectedEnemyPreset
-        //{
-        //    get { return _selectedEnemyPreset; }
-        //    set
-        //    {
-        //        _selectedEnemyPreset = value;
-        //        OnPropertyChanged(nameof(SelectedEnemyPreset));
-        //    }
-        //}
-
-        //private ICommand _usePresetCommand;
-        //public ICommand UsePresetCommand => _usePresetCommand ?? (_usePresetCommand = new CommandHandler(UsePreset, () => { return true; }));
-
-        //public void UsePreset()
-        //{
-        //    var spellSlots = new List<SpellSlotAvailabilityViewModel>();
-
-        //    foreach (var spellSlot in SelectedEnemyPreset.SpellSlots)
-        //    {
-        //        spellSlots.Add(new SpellSlotAvailabilityViewModel(spellSlot.Copy()));
-        //    }
-
-        //    Name = SelectedEnemyPreset.Name;
-        //    ArmourClass = SelectedEnemyPreset.ArmourClass;
-        //    Health = SelectedEnemyPreset.Health;
-        //    Speed = SelectedEnemyPreset.Speed;
-        //    Strength = SelectedEnemyPreset.Strength;
-        //    Dexterity = SelectedEnemyPreset.Dexterity;
-        //    Constitution = SelectedEnemyPreset.Constitution;
-        //    Intelligence = SelectedEnemyPreset.Intelligence;
-        //    Wisdom = SelectedEnemyPreset.Wisdom;
-        //    Charisma = SelectedEnemyPreset.Charisma;
-        //    IsSpellCaster = SelectedEnemyPreset.IsSpellCaster;
-        //    SpellSlots = spellSlots;
-        //    EditSavingThrowsViewModel = new EditTraitsWithModifierViewModel<AbilityScoreType>("Saving Throws: ", SelectedEnemyPreset.SavingThrows);
-        //    EditDamageVulnerabilitiesViewModel = new EditTraitsViewModel<DamageType>("Damage Vurnerabilities: ", SelectedEnemyPreset.DamageVurnerabilities);
-        //    EditDamageResistancesViewModel = new EditTraitsViewModel<DamageType>("Damage Resistances: ", SelectedEnemyPreset.DamageResistances);
-        //    EditDamageImmunitiesViewModel = new EditTraitsViewModel<DamageType>("Damage Immunities: ", SelectedEnemyPreset.DamageResistances);
-        //    EditConditionImmunitiesViewModel = new EditTraitsViewModel<Condition>("Condition Immunities: ", SelectedEnemyPreset.ConditionImmunities);
-        //    EditSkillsViewModel = new EditTraitsWithModifierViewModel<SkillType>("Skills: ", SelectedEnemyPreset.Skills);
-        //    EditSensesViewModel = new EditTraitsViewModel<SenseType>("Senses: ", SelectedEnemyPreset.Senses);
-        //    PassivePerception = new PassivePerceptionViewModel(SelectedEnemyPreset.PassivePerception);
-        //    EditLanguagesViewModel = new EditTraitsViewModel<LanguageType>("Languages: ", SelectedEnemyPreset.Languages);
-        //    ChallengeRatingViewModel = new ChallengeRatingViewModel(SelectedEnemyPreset.ChallengeRating.Copy());
-        //    EditAbilitiesViewModel = new EditAbilitiesViewModel(SelectedEnemyPreset.Abilities);
-        //    EditActionsViewModel = new EditActionsViewModel(_targetArmourClassProvider, _advantageDisadvantageProvider, SelectedEnemyPreset.Actions);
-        //    HealthRollViewModel.DiceNumber = SelectedEnemyPreset.HealthRoll.NumberOfDice;
-        //    HealthRollViewModel.DiceBase = SelectedEnemyPreset.HealthRoll.DiceBase;
-        //    HealthRollViewModel.ValueModifierViewModel = new ModifierViewModel(SelectedEnemyPreset.HealthRoll.ValueModifier.Copy());
-        //    InitiativeRollViewModel.ValueModifierViewModel = new ModifierViewModel(SelectedEnemyPreset.InitiativeRoll.ValueModifier.Copy());
-        //    LegendaryActionsDescription = SelectedEnemyPreset.LegendaryActionsDescription;
-        //    LairActionsDescription = SelectedEnemyPreset.LairActionsDescription;
-
-        //    OnPropertyChanged(string.Empty);
-        //}
-
-        //#endregion
 
         public EnemyInInitiativeViewModel AddedEnemy { get; set; }
 
