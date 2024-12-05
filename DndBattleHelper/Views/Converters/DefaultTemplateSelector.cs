1216befcd2a9b29da1d60bf59ca63003ad5789af
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2024-12-05 11:30:53 +0800

    Add dataTemplateSelector to add a default template so that generic traits are coloured

---
+++ DndBattleHelper/Views/Converters/DefaultTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DndBattleHelper.Views.Converters;
+
+public class DefaultTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate DefaultTemplate { get; set; }
+
+    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    {
+        if (item == null)
+            return DefaultTemplate;  // Fallback for null
+
+        // Use the default template if no specific template is found
+        FrameworkElement element = container as FrameworkElement;
+        var dataTemplate = element?.TryFindResource(new DataTemplateKey(item.GetType())) as DataTemplate;
+        return dataTemplate ?? DefaultTemplate;
+    }
+}
