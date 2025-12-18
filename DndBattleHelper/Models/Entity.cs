2f17bfcc7cd5ae817920c3fe11d8a26689967d25
laszpaulina <48447681+laszpaulina@users.noreply.github.com>
2025-12-18 22:22:55 +0000

    Add Armour class to player

--- DndBattleHelper/Models/Entity.cs
+++ DndBattleHelper/Models/Entity.cs
@@ -9,12 +9,14 @@ namespace DndBattleHelper.Models
         public int Initiative { get; set; }
         public string Name { get; set; }
         public int Health { get; set; }
+        public int ArmourClass { get; set; }
 
-        public Entity(int initiative, string name, int health)
+        public Entity(int initiative, string name, int health, int armourClass)
         {
             Initiative = initiative;
             Name = name;
             Health = health;
+            ArmourClass = armourClass;
         }
 
         public Entity() { }
